0a77186638597d390ed6b5bf6651ca40577fa17f
Alex Vernacchia <avernacchia@exacttarget.com>
2013-04-24 12:52:18 -0400

    minifying JS files and using minified backbone version

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ require.config({
 	paths: {
 		jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min'
 		, lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/1.2.0/lodash.min'
-		, backbone: 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone'
+		, backbone: 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min'
 		, mustache: 'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min'
 	}
 	, shim: {
