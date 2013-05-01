26505c5cc2f5a542f682da12fd60cacc1d8e81d5
Alex Vernacchia <avernacchia@exacttarget.com>
2013-05-01 15:46:11 -0400

    WIP changing way extra attributes were created in model

--- js/views/project.js
+++ js/views/project.js
@@ -13,17 +13,10 @@ define(
 		"use strict";
 		var ProjectsView = Backbone.View.extend({
 			initialize: function() {
-				this.createDesc();
+				this.listenTo(this.model, 'change', this.model.createInitialAttributes );
 			}
 			, render: function() {
 				return Mustache.render( ProjectTmpl, this.model );
-			}
-			, createDesc: function() {
-				if( this.model.get('description').length > 100 ) {
-					this.model.set('descriptionShort', this.model.get('description').substring(0, 100) + '...' );
-				} else {
-					this.model.set('descriptionShort', this.model.get('description') );
-				}
 			}
 		});
 		return ProjectsView;
